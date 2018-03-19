c36a9d519474fc200d196be5ad5264e93b532b3f
James Riall <jriall@google.com>
2018-03-19 10:01:57 +0000

    Completed services module

---
+++ src/app/hero.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
+import { MessageService } from './message.service';
+
+import { Hero } from './hero.model';
+import { HEROES } from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+
+}
